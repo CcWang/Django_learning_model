7e2af259362e39c91a16ba91fb37b64cc4d53d0f
CC Wang <chongchong1110@gmail.com>
2016-07-18 11:46:19 -0700

    changes

--- apps/interests/urls.py
+++ apps/interests/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^interests/$',views.show,name = 'show'),
-    url(r'^(?P<user_id>[0-9]+)/$',views.user, name='user')
+    url(r'^interests/(?P<user_id>[0-9]+)/$',views.user, name='user')
 ]
